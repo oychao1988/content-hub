8550c0fbfd6084dc82bb8be8849f161680553a28
ouyangchao <2296458423@qq.com>
2026-01-31 15:49:26 +0800

    添加 clearUserState 方法到返回对象

--- src/frontend/src/stores/modules/user.js
+++ src/frontend/src/stores/modules/user.js
@@ -97,6 +97,7 @@ export const useUserStore = defineStore(
       login,
       logout,
       getUserInfo,
+      clearUserState,
       hasPermission,
       hasAnyPermission,
       hasAllPermissions
